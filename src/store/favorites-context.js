09733cb15258be35c95175b3990bf1dcc8ed86d8
DesmondM <desmondmpofu@gmail.com>
2021-08-05 23:46:42 +0200

    favorites

--- src/store/favorites-context.js
+++ src/store/favorites-context.js
@@ -1,1 +1,23 @@
 import {createContext} from 'react';
+const FavoritesContext = createContext({
+    favorites: [],
+    totalFavorites: 0,
+    addFavorites: (favoriteMeetup) =>{},
+    removeFavorite: (meetupId) =>{},
+    itemIsFavorite: (meetupId)=>{}
+});
+
+function FavoritesContextProvider(props){
+    const [userFavorites, setUserFavorites] = useState([]);
+
+    function addFavoriteHandler(favoriteMeetup){
+        setUserFavorites((prevUserFavorites)=>{
+            return prevUserFavorites.concat(favoriteMeetup);
+        });
+    }
+    function removeFavoriteHandler(meetupId){
+        setUserFavorites(prevUserFavorites)=>{
+            return prevUserFavorites.filter(meetup => meetup.id !==meetupId);
+        }
+    }
+}
