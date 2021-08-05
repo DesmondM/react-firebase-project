90aea696fe5526e0cb12474441c3b2c83062188a
DesmondM <desmondmpofu@gmail.com>
2021-08-05 23:57:16 +0200

    context

--- src/store/favorites-context.js
+++ src/store/favorites-context.js
@@ -7,7 +7,7 @@ const FavoritesContext = createContext({
     itemIsFavorite: (meetupId)=>{}
 });
 
-function FavoritesContextProvider(props){
+export function FavoritesContextProvider(props){
     const [userFavorites, setUserFavorites] = useState([]);
 
     function addFavoriteHandler(favoriteMeetup){
@@ -18,6 +18,22 @@ function FavoritesContextProvider(props){
     function removeFavoriteHandler(meetupId){
         setUserFavorites(prevUserFavorites)=>{
             return prevUserFavorites.filter(meetup => meetup.id !==meetupId);
-        }
+        });
     }
+
+    function itemIsFavoriteHandler(meetupId){
+        return userFavorites.some(meetup=> meetup.id===meetupId);
+    }
+
+    const context = {
+        favorites: userFavorites,
+        totalFavorites: userFavorites.length,
+        addFavorites: addFavoriteHandler,
+        removeFavorite:removeFavoriteHandler,
+        itemIsFavorite:itemIsFavoriteHandler
+    };
+    return(
+    <FavoritesContext.Provider value={context}>
+        {props.children}
+    </FavoritesContext.Provider>);
 }
